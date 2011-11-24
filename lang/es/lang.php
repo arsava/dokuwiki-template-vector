cac4e5b2eb5281b19059050f5432489ed4e03cb4
Andreas Haerter <development@andreas-haerter.com>
2011-11-24 03:26:29 +0100

    added Spanish language files

---
+++ lang/es/lang.php
@@ -0,0 +1,105 @@
+<?php
+
+/**
+ * Spanish language for the "vector" DokuWiki template
+ *
+ * If your language is not/only partially translated or you found an error/typo,
+ * have a look at the following files:
+ * - "/lib/tpl/vector/lang/<your lang>/lang.php"
+ * - "/lib/tpl/vector/lang/<your lang>/settings.php"
+ * If they are not existing, copy and translate the English ones (hint: looking
+ * at <http://[your lang].wikipedia.org> might be helpful). And don't forget to
+ * mail the translation to me,
+ * Andreas Haerter <development@andreas-haerter.com>. Thanks :-D.
+ *
+ *
+ * LICENSE: This file is open source software (OSS) and may be copied under
+ *          certain conditions. See COPYING file for details or try to contact
+ *          the author(s) of this file in doubt.
+ *
+ * @license GPLv2 (http://www.gnu.org/licenses/gpl2.html)
+ * @author Jesús Muñoz Martínez <kisumum@gmail.com>
+ * @link http://andreas-haerter.com/projects/dokuwiki-template-vector
+ * @link http://www.dokuwiki.org/template:vector
+ * @link http://www.dokuwiki.org/config:lang
+ * @link http://www.dokuwiki.org/devel:configuration
+ */
+
+
+//check if we are running within the DokuWiki environment
+if (!defined("DOKU_INC")){
+    die();
+}
+
+//tabs, personal tools and special links
+$lang["vector_article"] = "Artículo";
+$lang["vector_discussion"] = "Discusión";
+$lang["vector_read"] = "Leer";
+$lang["vector_edit"] = "Editar";
+$lang["vector_create"] = "Crear";
+$lang["vector_userpage"] = "Página de Usuario";
+$lang["vector_specialpage"] = "Páginas Especiales";
+$lang["vector_mytalk"] = "Mi Discurso";
+$lang["vector_exportodt"] = "Exportar: ODT";
+$lang["vector_exportpdf"] = "Exportar: PDF";
+$lang["vector_subscribens"] = "Suscribirse a NS Cambios"; //original DW lang $lang["btn_subscribens"] is simply too long for common tab configs
+$lang["vector_unsubscribens"] = "Borrarse de NS Cambios";  //original DW lang $lang["btn_unsubscribens"] is simply too long for common tab configs
+$lang["vector_translations"] = "Idiomas";
+
+//headlines for the different bars and boxes
+$lang["vector_navigation"] = "Navegación";
+$lang["vector_toolbox"] = "Herramientas";
+$lang["vector_exportbox"] = "Imprimir/exportar";
+$lang["vector_inotherlanguages"] = "Idiomas";
+$lang["vector_printexport"] = "Imprimir/exportar";
+$lang["vector_personnaltools"] = "Herramientas Personales";
+
+//buttons
+$lang["vector_btn_go"] = "Ir";
+$lang["vector_btn_search"] = "Buscar";
+$lang["vector_btn_search_title"] = "Buscar este texto";
+
+//exportbox ("print/export")
+$lang["vector_exportbxdef_print"] = "Versión imprimible";
+$lang["vector_exportbxdef_downloadodt"] = "Descargar como ODT";
+$lang["vector_exportbxdef_downloadpdf"] = "Descargar como PDF";
+
+//default toolbox
+$lang["vector_toolbxdef_whatlinkshere"] = "Qué enlaza aquí";
+$lang["vector_toolbxdef_upload"] = "Cargar archivo";
+$lang["vector_toolbxdef_siteindex"] = "Índice";
+$lang["vector_toolboxdef_permanent"] = "Inicio";
+$lang["vector_toolboxdef_cite"] = "Citar esta página";
+
+//cite this article
+$lang["vector_cite_bibdetailsfor"] = "Detalles bibliográficos de";
+$lang["vector_cite_pagename"] = "Nombre de la página";
+$lang["vector_cite_author"] = "Autor";
+$lang["vector_cite_publisher"] = "Editor";
+$lang["vector_cite_dateofrev"] = "Día de esta revisión";
+$lang["vector_cite_dateretrieved"] = "Día recuperado";
+$lang["vector_cite_permurl"] = "URL Permanente";
+$lang["vector_cite_pageversionid"] = "ID de la Versión de la Página";
+$lang["vector_cite_citationstyles"] = "Estilos de cita para";
+$lang["vector_cite_checkstandards"] = "Por favor recuerda consultar el manual de estilo, guías de estándares o las directrices del instructor para una sintáxis exacta para satisfacer sus necesidades.";
+$lang["vector_cite_latexusepackagehint"] = "Cuando uses el paquete LaTeX url (\usepackage{url} en algún sitio en el preámbulo),tiende a dar el formato para las direcciones web, lo siguiente pueden ser preferido";
+$lang["vector_cite_retrieved"] = "Recuperado";
+$lang["vector_cite_from"] = "de";
+$lang["vector_cite_in"] = "En";
+$lang["vector_cite_accessed"] = "Accedido";
+$lang["vector_cite_cited"] = "Citado";
+$lang["vector_cite_lastvisited"] = "Último visitado";
+$lang["vector_cite_availableat"] = "Disponible en";
+$lang["vector_cite_discussionpages"] = "DokuWiki páginas de charla";
+$lang["vector_cite_markup"] = "Margen";
+$lang["vector_cite_result"] = "Resultado";
+$lang["vector_cite_thisversion"] = "esta versión";
+
+//other
+$lang["vector_search"] = "Buscar";
+$lang["vector_accessdenied"] = "Acceso denegado";
+$lang["vector_fillplaceholder"] = "Por favor rellena este marcador";
+$lang["vector_donate"] = "Donaciones";
+$lang["vector_mdtemplatefordw"] = "Plantilla vector para DokuWiki";
+$lang["vector_recentchanges"] = "Cambios recientes";
+
---
+++ lang/es/settings.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * Spanish language for the Config Manager
+ *
+ * If your language is not/only partially translated or you found an error/typo,
+ * have a look at the following files:
+ * - "/lib/tpl/vector/lang/<your lang>/lang.php"
+ * - "/lib/tpl/vector/lang/<your lang>/settings.php"
+ * If they are not existing, copy and translate the English ones (hint: looking
+ * at <http://[your lang].wikipedia.org> might be helpful). And don't forget to
+ * mail the translation to me,
+ * Andreas Haerter <development@andreas-haerter.com>. Thanks :-D.
+ *
+ *
+ * LICENSE: This file is open source software (OSS) and may be copied under
+ *          certain conditions. See COPYING file for details or try to contact
+ *          the author(s) of this file in doubt.
+ *
+ * @license GPLv2 (http://www.gnu.org/licenses/gpl2.html)
+ * @author Jesús Muñoz Martínez <kisumum@gmail.com>
+ * @link http://andreas-haerter.com/projects/dokuwiki-template-vector
+ * @link http://www.dokuwiki.org/template:vector
+ * @link http://www.dokuwiki.org/config:lang
+ * @link http://www.dokuwiki.org/devel:configuration
+ */
+
+
+//check if we are running within the DokuWiki environment
+if (!defined("DOKU_INC")){
+    die();
+}
+
+//user pages
+$lang["vector_userpage"]    = "Usar páginas de Usuario?";
+$lang["vector_userpage_ns"] = "Sí, usa la siguiente ':namespace:' como raíz para las páginas de usuario:";
+
+//discussion pages
+$lang["vector_discuss"]    = "¿Usar pestañas/sitios de discusión?";
+$lang["vector_discuss_ns"] = "Sí, usa la siguiente ':namespace:' como raíz para las discusiones:";
+
+//site notice
+$lang["vector_sitenotice"]          = "Show site wide notice?";
+$lang["vector_sitenotice_location"] = "If yes, use following wiki page for the site wide notice:";
+
+//navigation
+$lang["vector_navigation"]          = "¿Mostrar navegación?";
+$lang["vector_navigation_location"] = "Sí, usar la siguiente página de la wiki como navegación:";
+
+//exportbox ("print/export")
+$lang["vector_exportbox"]          = "¿Mostrar la caja 'imprimir/exportar'?";
+$lang["vector_exportbox_default"]  = "Sí, usar la caja predeterminada'imprimir/exportar' por defecto";
+$lang["vector_exportbox_location"] = "si no usar predeterminado, usar la siguiente página de la wiki como localización de la caja'imprimir/exportar':";
+
+//toolbox
+$lang["vector_toolbox"]          = "¿Mostrar herramientas?";
+$lang["vector_toolbox_default"]  = "Si, usar herramientas predeterminadas";
+$lang["vector_toolbox_location"] = "si no usar predeterminado, usar la siguiente página de la wiki como localización de las herramientas:";
+
+//custom copyright notice
+$lang["vector_copyright"]          = "¿Mostrar aviso de derechos de autor?";
+$lang["vector_copyright_default"]  = "Sí, usar aviso de derechos de autor predeterminado";
+$lang["vector_copyright_location"] = "si no usar predeterminado, usar la siguiente página de la wiki como aviso de derechos de autor:";
+
+//donation link/button
+$lang["vector_donate"]          = "Mostrar enlace/botón de donación?";
+$lang["vector_donate_default"]  = "Sí, usar URL predeterminada para las donaciones";
+$lang["vector_donate_url"]      = "si no usar predeterminado, usar la siguiente URL para las donaciones:";
+
+//TOC
+$lang["vector_toc_position"] = "Posición de la Tabla de Contenidos";
+
+//other stuff
+$lang["vector_mediamanager_embedded"] = "Mostar mediamanger empotrado en el diseño estandar?";
+$lang["vector_breadcrumbs_position"]  = "Posición de breadcrumb en navegación (si habilitada):";
+$lang["vector_youarehere_position"]   = "Posicion de  'Tu estás aquí' en navegación (si habilitada):";
+$lang["vector_cite_author"]           = "Nombre del author en  'Cita este Artículo':";
+$lang["vector_loaduserjs"]            = "¿Cargar 'vector/user/user.js'?";
+$lang["vector_closedwiki"]            = "¿Wiki cerrada?.(la mayoría de enlaces/pestañas/cajas están escondidas hasta que el usuario se conecta)";
+
