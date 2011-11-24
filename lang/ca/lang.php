9ee1331858b4b6e7e7b54edd69a13b39af2bfd65
Andreas Haerter <development@andreas-haerter.com>
2011-11-24 02:17:23 +0100

    added Catalan language files

---
+++ lang/ca/lang.php
@@ -0,0 +1,104 @@
+<?php
+
+/**
+ * Catalan language for the "vector" DokuWiki template
+ *
+ * If your language is not/only partially translated or you found an error/typo,
+ * have a look at the following files:
+ * - "/lib/tpl/vector/lang/<your lang>/lang.php"
+ * - "/lib/tpl/vector/lang/<your lang>/settings.php"
+ * If they are not existing, copy and translate the English ones (hint: looking
+ * at <http://[your lang].wikipedia.org> might be helpful). And don't forget to
+ * mail the translation to me,
+ * Andreas Haerter <development@andreas-haerter.com>. Thanks :-D.
+ *
+ *
+ * LICENSE: This file is open source software (OSS) and may be copied under
+ *          certain conditions. See COPYING file for details or try to contact
+ *          the author(s) of this file in doubt.
+ *
+ * @license GPLv2 (http://www.gnu.org/licenses/gpl2.html)
+ * @author Albert Gasset Romo <albert.gasset@gmail.com>
+ * @link http://andreas-haerter.com/projects/dokuwiki-template-vector
+ * @link http://www.dokuwiki.org/template:vector
+ * @link http://www.dokuwiki.org/config:lang
+ * @link http://www.dokuwiki.org/devel:configuration
+ */
+
+
+//check if we are running within the DokuWiki environment
+if (!defined("DOKU_INC")){
+    die();
+}
+
+//tabs, personal tools and special links
+$lang["vector_article"] = "Pàgina";
+$lang["vector_discussion"] = "Discussió";
+$lang["vector_read"] = "Llegeix";
+$lang["vector_edit"] = "Edita";
+$lang["vector_create"] = "Crea";
+$lang["vector_userpage"] = "Pàgina d'usuari";
+$lang["vector_specialpage"] = "Pàgines especials";
+$lang["vector_mytalk"] = "Discussió";
+$lang["vector_exportodt"] = "Exporta a ODT";
+$lang["vector_exportpdf"] = "Exporta a PDF";
+$lang["vector_subscribens"] = "Subscriu als canvis de l'espai"; //original DW lang $lang["btn_subscribens"] is simply too long for common tab configs
+$lang["vector_unsubscribens"] = "Cancel·la subscripció a l'espai";  //original DW lang $lang["btn_unsubscribens"] is simply too long for common tab configs
+$lang["vector_translations"] = "Llengües";
+
+//headlines for the different bars and boxes
+$lang["vector_navigation"] = "Navegació";
+$lang["vector_toolbox"] = "Quadre d'eines";
+$lang["vector_exportbox"] = "Imprimeix/exporta";
+$lang["vector_inotherlanguages"] = "Llengües";
+$lang["vector_printexport"] = "Imprimeix/exporta";
+$lang["vector_personnaltools"] = "Eines personals";
+
+//buttons
+$lang["vector_btn_go"] = "Vés";
+$lang["vector_btn_search"] = "Cerca";
+$lang["vector_btn_search_title"] = "Cerca aquest text";
+
+//exportbox ("print/export")
+$lang["vector_exportbxdef_print"] = "Versió per a impressió";
+$lang["vector_exportbxdef_downloadodt"] = "Baixa en format ODT";
+$lang["vector_exportbxdef_downloadpdf"] = "Baixa en format PDF";
+
+//default toolbox
+$lang["vector_toolbxdef_whatlinkshere"] = "Què hi enllaça";
+$lang["vector_toolbxdef_upload"] = "Gestor de fitxers";
+$lang["vector_toolbxdef_siteindex"] = "Índex del lloc";
+$lang["vector_toolboxdef_permanent"] = "Enllaç permanent";
+$lang["vector_toolboxdef_cite"] = "Cita aquesta pàgina";
+
+//cite this article
+$lang["vector_cite_bibdetailsfor"] = "Detalls bibliogràfics per a";
+$lang["vector_cite_pagename"] = "Nom de la pàgina";
+$lang["vector_cite_author"] = "Autor";
+$lang["vector_cite_publisher"] = "Editorial";
+$lang["vector_cite_dateofrev"] = "Data d'aquesta revisió";
+$lang["vector_cite_dateretrieved"] = "Data de recuperació";
+$lang["vector_cite_permurl"] = "URL permanent";
+$lang["vector_cite_pageversionid"] = "ID de la versió de la pàgina";
+$lang["vector_cite_citationstyles"] = "Estils de cita per a";
+$lang["vector_cite_checkstandards"] = "Si us plau, recordeu de comprar el vostre manual d'estil, guia de normes o directrius de l'instructor per a la sintaxi exacta per a les vostres necessitats.";
+$lang["vector_cite_latexusepackagehint"] = "Si feu servir el paquet de LaTeX url (\usepackage{url} en algun lloc del preàmbul), que tendeix a donar un format més agradable a les adreces web, potser preferiu el següent";
+$lang["vector_cite_retrieved"] = "Recuperat";
+$lang["vector_cite_from"] = "de";
+$lang["vector_cite_in"] = "A";
+$lang["vector_cite_accessed"] = "Accecit";
+$lang["vector_cite_cited"] = "Citat";
+$lang["vector_cite_lastvisited"] = "Visitat";
+$lang["vector_cite_availableat"] = "Disponible a";
+$lang["vector_cite_discussionpages"] = "Pàgines de discussió de la DokuWiki";
+$lang["vector_cite_markup"] = "Etiquetage";
+$lang["vector_cite_result"] = "Resultat";
+$lang["vector_cite_thisversion"] = "aquesta versió";
+
+//other
+$lang["vector_search"] = "Cerca";
+$lang["vector_accessdenied"] = "Accés denegat";
+$lang["vector_fillplaceholder"] = "Si us plau, ompliu aquest text variable";
+$lang["vector_donate"] = "Fes una donació";
+$lang["vector_mdtemplatefordw"] = "Plantilla vector per a DokuWiki";
+$lang["vector_recentchanges"] = "Canvis recents";
---
+++ lang/ca/settings.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * Catalan language for the Config Manager
+ *
+ * If your language is not/only partially translated or you found an error/typo,
+ * have a look at the following files:
+ * - "/lib/tpl/vector/lang/<your lang>/lang.php"
+ * - "/lib/tpl/vector/lang/<your lang>/settings.php"
+ * If they are not existing, copy and translate the English ones (hint: looking
+ * at <http://[your lang].wikipedia.org> might be helpful). And don't forget to
+ * mail the translation to me,
+ * Andreas Haerter <development@andreas-haerter.com>. Thanks :-D.
+ *
+ *
+ * LICENSE: This file is open source software (OSS) and may be copied under
+ *          certain conditions. See COPYING file for details or try to contact
+ *          the author(s) of this file in doubt.
+ *
+ * @license GPLv2 (http://www.gnu.org/licenses/gpl2.html)
+ * @author Albert Gasset Romo <albert.gasset@gmail.com>
+ * @link http://andreas-haerter.com/projects/dokuwiki-template-vector
+ * @link http://www.dokuwiki.org/template:vector
+ * @link http://www.dokuwiki.org/config:lang
+ * @link http://www.dokuwiki.org/devel:configuration
+ */
+
+
+//check if we are running within the DokuWiki environment
+if (!defined("DOKU_INC")){
+    die();
+}
+
+//user pages
+$lang["vector_userpage"]    = "Habilita les pàgines d'usuari";
+$lang["vector_userpage_ns"] = "Espai arrel de les pàgines d'usauri";
+
+//discussion pages
+$lang["vector_discuss"]    = "Habilita les discussions";
+$lang["vector_discuss_ns"] = "Espai arrel de les discussions";
+
+//site notice
+$lang["vector_sitenotice"]          = "Mostra l'avís del lloc";
+$lang["vector_sitenotice_location"] = "Pàgina de l'avís del lloc";
+
+//navigation
+$lang["vector_navigation"]          = "Mostra la navegació";
+$lang["vector_navigation_location"] = "Pàgina de la navegació";
+
+//exportbox ("print/export")
+$lang["vector_exportbox"]          = "Mostra el quadre 'imprimeix/exporta'";
+$lang["vector_exportbox_default"]  = "Quadre 'imprimeix/exporta' predeterminat";
+$lang["vector_exportbox_location"] = "Pàgina del quadre 'imprimeix/exporta'";
+
+//toolbox
+$lang["vector_toolbox"]          = "Mostra el quadre d'eines";
+$lang["vector_toolbox_default"]  = "Quadre d'eines predeterminat";
+$lang["vector_toolbox_location"] = "Pàgina del quadre d'eines";
+
+//custom copyright notice
+$lang["vector_copyright"]          = "Mostra l'avís de drets d'autor";
+$lang["vector_copyright_default"]  = "Avís de drets d'autor predeterminat";
+$lang["vector_copyright_location"] = "Pàgina de l'avís de drets d'autor";
+
+//donation link/button
+$lang["vector_donate"]          = "Mostra l'enllaç/botó de donacions";
+$lang["vector_donate_default"]  = "URL de donacions predeterminat";
+$lang["vector_donate_url"]      = "URL de donacions:";
+
+//TOC
+$lang["vector_toc_position"] = "Posició de la taule de continguts";
+
+//other stuff
+$lang["vector_mediamanager_embedded"] = "Mostra el gestor de fitxers incrustat";
+$lang["vector_breadcrumbs_position"]  = "Posició del camí de navegació";
+$lang["vector_youarehere_position"]   = "Posició de la navegació 'sou aquí'";
+$lang["vector_cite_author"]           = "Nom de l'autor a 'cita aquest article'";
+$lang["vector_loaduserjs"]            = "Carrega 'vector/user/user.js'";
+$lang["vector_closedwiki"]            = "Wiki tancada (la majoria d'enllaços/pestanyes/quadres s'amaguen fins que l'usuari entri)";
+
