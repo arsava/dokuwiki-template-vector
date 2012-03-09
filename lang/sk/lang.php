d371e543bff070331deb59b11a758ac74dc4b9ca
Andreas Haerter <development@andreas-haerter.com>
2012-03-09 14:32:49 +0100

    add Slovak language files

---
+++ lang/sk/lang.php
@@ -0,0 +1,105 @@
+﻿<?php
+
+/**
+ * Slovak language for the "vector" DokuWiki template
+ *
+ * If your language is not/only partially translated or you found an error/typo,
+ * have a look at the following files:
+ * - "/lib/tpl/vector/lang/<your lang>/lang.php"
+ * - "/lib/tpl/vector/lang/<your lang>/settings.php"
+ * If they are not existing, copy and translate the English ones (hint: looking
+ * at <http://[your lang].wikipedia.org> might be helpful). And don't forget to
+ * mail the translation to me,
+ * Andreas Haerter <development@andreas-haerter.com>. Thanks :-D.
+ *
+ *
+ * LICENSE: This file is open source software (OSS) and may be copied under
+ *          certain conditions. See COPYING file for details or try to contact
+ *          the author(s) of this file in doubt.
+ *
+ * @license GPLv2 (http://www.gnu.org/licenses/gpl2.html)
+ * @author Peter Bezemek
+ * @link http://andreas-haerter.com/projects/dokuwiki-template-vector
+ * @link http://www.dokuwiki.org/template:vector
+ * @link http://www.dokuwiki.org/config:lang
+ * @link http://www.dokuwiki.org/devel:configuration
+ */
+
+
+//check if we are running within the DokuWiki environment
+if (!defined("DOKU_INC")){
+    die();
+}
+
+//tabs, personal tools and special links
+$lang["vector_article"] = "Článok";
+$lang["vector_discussion"] = "Diskusia";
+$lang["vector_read"] = "Čítať";
+$lang["vector_edit"] = "Upraviť";
+$lang["vector_create"] = "Vytvoriť";
+$lang["vector_userpage"] = "Stránka Používateľa";
+$lang["vector_specialpage"] = "Špeciálne stránky";
+$lang["vector_mytalk"] = "Moje Diskusie";
+$lang["vector_exportodt"] = "Export: ODT";
+$lang["vector_exportpdf"] = "Export: PDF";
+$lang["vector_subscribens"] = "Prihlásiť odber zmien"; //original DW lang $lang["btn_subscribens"] is simply too long for common tab configs
+$lang["vector_unsubscribens"] = "Odhlásiť odber zmien";  //original DW lang $lang["btn_unsubscribens"] is simply too long for common tab configs
+$lang["vector_translations"] = "Jazyky";
+
+//headlines for the different bars and boxes
+$lang["vector_navigation"] = "Navigácia";
+$lang["vector_toolbox"] = "Nástroje";
+$lang["vector_exportbox"] = "Tlač/export";
+$lang["vector_inotherlanguages"] = "Jazyky";
+$lang["vector_printexport"] = "Tlač/export";
+$lang["vector_personnaltools"] = "Osobné Nástroje";
+
+//buttons
+$lang["vector_btn_go"] = "Spustiť";
+$lang["vector_btn_search"] = "Hľadať";
+$lang["vector_btn_search_title"] = "Hľadať tento text";
+
+//exportbox ("print/export")
+$lang["vector_exportbxdef_print"] = "Verzia pre tlač";
+$lang["vector_exportbxdef_downloadodt"] = "Stiahnuť ako ODT";
+$lang["vector_exportbxdef_downloadpdf"] = "Stiahnuť ako PDF";
+
+//default toolbox
+$lang["vector_toolbxdef_whatlinkshere"] = "Čo sem odkazuje";
+$lang["vector_toolbxdef_upload"] = "Nahrať súbor";
+$lang["vector_toolbxdef_siteindex"] = "Index stránok";
+$lang["vector_toolboxdef_permanent"] = "Trvalý odkaz";
+$lang["vector_toolboxdef_cite"] = "Citovať túto stránku";
+
+//cite this article
+$lang["vector_cite_bibdetailsfor"] = "Bibliografické podrobnosti pre";
+$lang["vector_cite_pagename"] = "Názov stránky";
+$lang["vector_cite_author"] = "Autor";
+$lang["vector_cite_publisher"] = "Vydavateľ";
+$lang["vector_cite_dateofrev"] = "Dátum tejto revízie";
+$lang["vector_cite_dateretrieved"] = "Dátum načítania";
+$lang["vector_cite_permurl"] = "Trvalé URL";
+$lang["vector_cite_pageversionid"] = "Identifikátor Verzie Stránky";
+$lang["vector_cite_citationstyles"] = "Štýly citácie pre";
+$lang["vector_cite_checkstandards"] = "Prosím nezabudnite konzultovať príručku štýlu, normy sprievodcu alebo inštrukcie pre presnú syntax, ktorá vyhovuje vaším potrebám.";
+$lang["vector_cite_latexusepackagehint"] = "Ak používate URL balíka LaTeX (\usepackage{url} niekde v preambule), ktorý zvykne dávať oveľa krajšie sformátované webové adresy, vhodnejšie môže byť nasledujúce:";
+$lang["vector_cite_retrieved"] = "Načítané";
+$lang["vector_cite_from"] = "z";
+$lang["vector_cite_in"] = "V";
+$lang["vector_cite_accessed"] = "Prístup";
+$lang["vector_cite_cited"] = "Citované";
+$lang["vector_cite_lastvisited"] = "Posledná návšteva";
+$lang["vector_cite_availableat"] = "K dispozícii na";
+$lang["vector_cite_discussionpages"] = "Diskusné stránky DokuWiki";
+$lang["vector_cite_markup"] = "Anotácia";
+$lang["vector_cite_result"] = "Výsledok";
+$lang["vector_cite_thisversion"] = "táto verzia";
+
+//other
+$lang["vector_search"] = "Hľadať";
+$lang["vector_accessdenied"] = "Prístup zamietnutý";
+$lang["vector_fillplaceholder"] = "Vyplňte prosím tento zástupný symbol";
+$lang["vector_donate"] = "Darujte";
+$lang["vector_mdtemplatefordw"] = "Šablóna Vector pre DokuWiki";
+$lang["vector_recentchanges"] = "Nedávne zmeny";
+
---
+++ lang/sk/settings.php
@@ -0,0 +1,80 @@
+﻿<?php
+
+/**
+ * Slovak language for the Config Manager
+ *
+ * If your language is not/only partially translated or you found an error/typo,
+ * have a look at the following files:
+ * - "/lib/tpl/vector/lang/<your lang>/lang.php"
+ * - "/lib/tpl/vector/lang/<your lang>/settings.php"
+ * If they are not existing, copy and translate the English ones (hint: looking
+ * at <http://[your lang].wikipedia.org> might be helpful). And don't forget to
+ * mail the translation to me,
+ * Andreas Haerter <development@andreas-haerter.com>. Thanks :-D.
+ *
+ *
+ * LICENSE: This file is open source software (OSS) and may be copied under
+ *          certain conditions. See COPYING file for details or try to contact
+ *          the author(s) of this file in doubt.
+ *
+ * @license GPLv2 (http://www.gnu.org/licenses/gpl2.html)
+ * @author Peter Bezemek
+ * @link http://andreas-haerter.com/projects/dokuwiki-template-vector
+ * @link http://www.dokuwiki.org/template:vector
+ * @link http://www.dokuwiki.org/config:lang
+ * @link http://www.dokuwiki.org/devel:configuration
+ */
+
+
+//check if we are running within the DokuWiki environment
+if (!defined("DOKU_INC")){
+    die();
+}
+
+//user pages
+$lang["vector_userpage"]    = "Používať Stránky Používateľa?";
+$lang["vector_userpage_ns"] = "Ak áno, použite nasledujúci ':namespace:' ako koreňový adresár pre používateľské stránky:";
+
+//discussion pages
+$lang["vector_discuss"]    = "Používať záložku/stránky Diskusia?";
+$lang["vector_discuss_ns"] = "Ak áno, použite nasledujúci ':namespace:' ako koreňový adresár pre používateľské stránky:";
+
+//site notice
+$lang["vector_sitenotice"]          = "Zobrazovať všeobecnú informáciu (na všetkých stránkach)?";
+$lang["vector_sitenotice_location"] = "Ak áno, pre definíciu všeobecnej informácie použite nasledujúcu wiki stránku:";
+
+//navigation
+$lang["vector_navigation"]           = "Zobrazovať navigáciu?";
+$lang["vector_navigation_location"]  = "Ak áno, pre definíciu navigácie použite nasledujúcu wiki stránku:";
+$lang["vector_navigation_translate"] = "Ak áno a <a href=\"http://www.dokuwiki.org/plugin:translation\">Translation plugin</a> je nainštalovaný: použiť navigáciu špecifickú pre daný jazyk?<br />Wiki stránka pre preloženú navigáciu je [hodnota pre 'vector_navigation_location']_[iso kód jazyka] (napr. ':wiki:navigation_sk').";
+
+//exportbox ("print/export")
+$lang["vector_exportbox"]          = "Zobrazovať 'tlač/export'?";
+$lang["vector_exportbox_default"]  = "Ak áno, používať základné nastavenie pre 'tlač/export'?";
+$lang["vector_exportbox_location"] = "Ak nie základné, používať pre definíciu funkcie 'tlač/export' nasledujúce umiestnenie:";
+
+//toolbox
+$lang["vector_toolbox"]          = "Zobrazovať nástroje?";
+$lang["vector_toolbox_default"]  = "Ak áno, používať základné nastavenie pre nástroje?";
+$lang["vector_toolbox_location"] = "Ak nie základné, používať pre definíciu funkcie Nástroje nasledujúce umiestnenie:";
+
+//custom copyright notice
+$lang["vector_copyright"]          = "Zobrazovať správu o copyrighte?";
+$lang["vector_copyright_default"]  = "Ak áno, používať základné nastavenie pre copyright?";
+$lang["vector_copyright_location"] = "Ak nie základné, používať pre definíciu copyrightu nasledujúce umiestnenie:";
+
+//donation link/button
+$lang["vector_donate"]          = "Zobrazovať odkaz/tlačidlo pre dary/dotácie?";
+$lang["vector_donate_default"]  = "Ak áno, používať základné nastavenie darovacie URL?";
+$lang["vector_donate_url"]      = "Ak nie základné, používať pre dary nasledujúce URL:";
+
+//TOC
+$lang["vector_toc_position"] = "Pozícia Obsahu";
+
+//other stuff
+$lang["vector_breadcrumbs_position"]  = "Pozícia omrvinkovej navigácie (ak je povolená):";
+$lang["vector_youarehere_position"]   = "Pozícia navigácie 'Nachádzate sa tu' (ak je povolená):";
+$lang["vector_cite_author"]           = "Meno autora v 'Citujte tento Článok':";
+$lang["vector_loaduserjs"]            = "Nahrať 'vector/user/user.js'?";
+$lang["vector_closedwiki"]            = "Uzamknutá wiki (väčšina odkazov/záložiek/nástrojov je skrytá, až kým sa používateľ neprihlási)?";
+
