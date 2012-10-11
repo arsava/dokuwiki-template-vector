93355e9b0aa87bb82cc0e31a418a12aa8f969a9f
Andreas Haerter <ah@bitkollektiv.org>
2012-10-12 01:30:39 +0200

    Add Persian language files

---
+++ lang/fa/lang.php
@@ -0,0 +1,103 @@
+<?php
+
+/**
+ * Persian language for the "vector" DokuWiki template
+ *
+ * If your language is not/only partially translated or you found an error/typo,
+ * have a look at the following files:
+ * - "/lib/tpl/vector/lang/<your lang>/lang.php"
+ * - "/lib/tpl/vector/lang/<your lang>/settings.php"
+ * If they are not existing, copy and translate the English ones (hint: looking
+ * at <http://[your lang].wikipedia.org> might be helpful). And don't forget to
+ * mail the translation to me,
+ * Andreas Haerter <ah@bitkollektiv.org>. Thanks :-D.
+ *
+ *
+ * LICENSE: This file is free software and may be copied under
+ *          certain conditions. See COPYING file for details or try to contact
+ *          the author(s) of this file in doubt.
+ *
+ * @license GPLv2 (http://www.gnu.org/licenses/gpl2.html)
+ * @author Salman Mohammadi <salman@shimool.org>
+ * @link http://www.dokuwiki.org/template:vector
+ * @link http://www.dokuwiki.org/config:lang
+ * @link http://www.dokuwiki.org/devel:configuration
+ */
+
+
+//check if we are running within the DokuWiki environment
+if (!defined("DOKU_INC")){
+    die();
+}
+
+//tabs, personal tools and special links
+$lang["vector_article"] = "مقاله";
+$lang["vector_discussion"] = "بحث";
+$lang["vector_read"] = "خواندن";
+$lang["vector_edit"] = "ویرایش";
+$lang["vector_create"] = "ساختن";
+$lang["vector_userpage"] = "صفحه‌ی کاربر";
+$lang["vector_specialpage"] = "صفحه‌های ویژه";
+$lang["vector_mytalk"] = "گفتگوی کم";
+$lang["vector_exportodt"] = "بارگیری به صورت ODT";
+$lang["vector_exportpdf"] = "بارگیری به صورت PDF";
+$lang["vector_subscribens"] = "Subscribe NS Changes"; //original DW lang $lang["btn_subscribens"] is simply too long for common tab configs
+$lang["vector_unsubscribens"] = "Unsubscribe NS Changes";  //original DW lang $lang["btn_unsubscribens"] is simply too long for common tab configs
+$lang["vector_translations"] = "زبان‌ها";
+
+//headlines for the different bars and boxes
+$lang["vector_navigation"] = "ناوبری";
+$lang["vector_toolbox"] = "جعبه ابزار";
+$lang["vector_exportbox"] = "چاپ/برون‌بری";
+$lang["vector_inotherlanguages"] = "زبان‌ها";
+$lang["vector_printexport"] = "چاپ/برون‌بری";
+$lang["vector_personnaltools"] = "ابزارهای شخصی";
+
+//buttons
+$lang["vector_btn_go"] = "برو";
+$lang["vector_btn_search"] = "جست‌و‌جو";
+$lang["vector_btn_search_title"] = "جست‌و‌جو برای این متن";
+
+//exportbox ("print/export")
+$lang["vector_exportbxdef_print"] = "نسخه‌ی قابل چاپ";
+$lang["vector_exportbxdef_downloadodt"] = "بارگیری به صورت ODT";
+$lang["vector_exportbxdef_downloadpdf"] = "بارگیری به صورت PDF";
+
+//default toolbox
+$lang["vector_toolbxdef_whatlinkshere"] = "پیوندهای به این صفحه";
+$lang["vector_toolbxdef_upload"] = "بارگذاری پرونده";
+$lang["vector_toolbxdef_siteindex"] = "شاخص وب‌گاه";
+$lang["vector_toolboxdef_permanent"] = "پیوند پایدار";
+$lang["vector_toolboxdef_cite"] = "یادکرد این صفحه";
+
+//cite this article
+$lang["vector_cite_bibdetailsfor"] = "جزییات کتاب‌شناسی برای";
+$lang["vector_cite_pagename"] = "نام صفحه";
+$lang["vector_cite_author"] = "نویسنده";
+$lang["vector_cite_publisher"] = "منتشر کننده";
+$lang["vector_cite_dateofrev"] = "تاریخ آخرین بازبینی";
+$lang["vector_cite_dateretrieved"] = "تاریخ بازیابی";
+$lang["vector_cite_permurl"] = "پیوند دایمی";
+$lang["vector_cite_pageversionid"] = "شناسه ویرایش صفحه";
+$lang["vector_cite_citationstyles"] = "شیوه‌های یادکرد برای";
+$lang["vector_cite_checkstandards"] = "لطفاُ به یاد داشته باشید که برای سینتکس دقیق که مطابق با نیازهای شما باشد، باید به دفترچه‌راهنمای شیوه، راهنماهای استاندارد و یا دیگر راهنماها مراجعه کنید";
+$lang["vector_cite_latexusepackagehint"] = "When using the LaTeX package url (\usepackage{url} somewhere in the preamble), which tends to give much more nicely formatted web addresses, the following may be preferred";
+$lang["vector_cite_retrieved"] = "بازبینی‌شده";
+$lang["vector_cite_from"] = "از";
+$lang["vector_cite_in"] = "در";
+$lang["vector_cite_accessed"] = "بازبینی شده";
+$lang["vector_cite_cited"] = "یاد شده";
+$lang["vector_cite_lastvisited"] = "آخرین دیدار";
+$lang["vector_cite_availableat"] = "موجود در";
+$lang["vector_cite_discussionpages"] = "صفحه‌های گفتگوی دوکوویکی";
+$lang["vector_cite_markup"] = "نشانه‌گذاری";
+$lang["vector_cite_result"] = "نتیجه";
+$lang["vector_cite_thisversion"] = "این ویرایش";
+
+//other
+$lang["vector_search"] = "جست‌و‌جو";
+$lang["vector_accessdenied"] = "دسترسی محدود شده است";
+$lang["vector_fillplaceholder"] = "Please fill this placeholder";
+$lang["vector_donate"] = "اهدا کردن";
+$lang["vector_mdtemplatefordw"] = "الگوی «وکتور» برای دوکوویکی";
+$lang["vector_recentchanges"] = "تغییرهای اخیر";
---
+++ lang/fa/settings.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * Persian language for the Config Manager
+ *
+ * If your language is not/only partially translated or you found an error/typo,
+ * have a look at the following files:
+ * - "/lib/tpl/vector/lang/<your lang>/lang.php"
+ * - "/lib/tpl/vector/lang/<your lang>/settings.php"
+ * If they are not existing, copy and translate the English ones (hint: looking
+ * at <http://[your lang].wikipedia.org> might be helpful). And don't forget to
+ * mail the translation to me,
+ * Andreas Haerter <ah@bitkollektiv.org>. Thanks :-D.
+ *
+ *
+ * LICENSE: This file is free software and may be copied under
+ *          certain conditions. See COPYING file for details or try to contact
+ *          the author(s) of this file in doubt.
+ *
+ * @license GPLv2 (http://www.gnu.org/licenses/gpl2.html)
+ * @author Salman Mohammadi <salman@shimool.org>
+ * @link http://www.dokuwiki.org/template:vector
+ * @link http://www.dokuwiki.org/config:lang
+ * @link http://www.dokuwiki.org/devel:configuration
+ */
+
+
+//check if we are running within the DokuWiki environment
+if (!defined("DOKU_INC")){
+    die();
+}
+
+//user pages
+$lang["vector_userpage"]    = "از صفحه‌ی کاربر استفاده شود؟";
+$lang["vector_userpage_ns"] = "اگر بله، از :namespace: زیر به عنوان ریشه برای صفحه‌های کاربرها استفاده کن:";
+
+//discussion pages
+$lang["vector_discuss"]    = "از وب‌گاه‌های/برگه‌های بحث استفاده شود؟";
+$lang["vector_discuss_ns"] = "اگر بله، از :namespace: زیر به عنوان ریشه یرای صفحه‌های بحث استفاده کن:";
+
+//site notice
+$lang["vector_sitenotice"]          = "آیا از اعلامیه‌ی سراسری در وب‌گاه استفاده شود؟";
+$lang["vector_sitenotice_location"] = ":اگر بله، از صفحه‌ی ویکی زیر برای اعلامیه‌ی سراسری در وب‌گاه استفاده کن";
+
+//navigation
+$lang["vector_navigation"]           = "ناوبری نشان داده شود؟";
+$lang["vector_navigation_location"]  = ":اگر بله، از صفحه‌ی ویکی زیر برای ناوبری استفاده کن";
+$lang["vector_navigation_translate"] = "اگر بله و <a href=\"http://www.dokuwiki.org/plugin:translation\">Translation plugin</a> در دسترس بود: آیا آیا از ناوبری مخصوص هر زبان استفاده شود؟<br />صفحه‌ی ویرایش برای ناوبری(های) ترجمه شده به مانند روبرو است [مقدار 'vector_navigation_location']_[iso lang code] (مثلا ':wiki:navigation_fa').";
+
+//exportbox ("print/export")
+$lang["vector_exportbox"]          = "جعبه‌ی «چاپ/برون‌ریزی را نشان دهد؟?";
+$lang["vector_exportbox_default"]  = "اگر بله، از جعبه‌ی «چاپ/برون‌ریزی» پیش‌فرض استفاده کند؟";
+$lang["vector_exportbox_location"] = "اگر نمی‌خواهید از پیش‌فرض استفاده کند، از صفحه‌ی ویکی زیر به عنوان مکان «چاپ/برون‌ریزی» استفاده کند.";
+
+//toolbox
+$lang["vector_toolbox"]          = "جعبه‌ابزار را نشان دهد؟";
+$lang["vector_toolbox_default"]  = "اگر بله، از جعبه‌‌ابزار  پیش‌فرض استفاده کند؟";
+$lang["vector_toolbox_location"] = "اگر نمی‌خواهید از پیش‌فرض استفاده کند، از صفحه‌ی ویکی زیر به عنوان مکان جعبه‌ابزار استفاده کند.";
+
+//custom copyright notice
+$lang["vector_copyright"]          = "آیا اعلامیه‌ی حق‌کپی را نشان دهد؟";
+$lang["vector_copyright_default"]  = "اگر بله، از اعلامیه‌ی حق کپی پیش‌فرض استفاده کند؟";
+$lang["vector_copyright_location"] = "اگر نمی‌خواهید از پیش‌فرض استفاده کند، از صفحه‌ی ویکی زیر به عنوان اعلامیه‌ی حق‌کپی استفاده کند.";
+
+//donation link/button
+$lang["vector_donate"]          = "پیوند/دکمه‌ی اهدا را نشان دهد؟";
+$lang["vector_donate_default"]  = "اگر بله، از پیوند پیش‌فرض برای اهدا استفاده کند؟";
+$lang["vector_donate_url"]      = "اگر نمی‌خواهید از پیش‌فرض استفاده کند، از پیوند زیر برای اهدا استفاده کند:.";
+
+//TOC
+$lang["vector_toc_position"] = "موقعیت فهرست مطالب";
+
+//other stuff
+$lang["vector_breadcrumbs_position"]  = "موقعیت ناوبری بردکرامب (اگر فعال شده باشد):";
+$lang["vector_youarehere_position"]   = "موقعیت «شما اینجا هستید» (اگر فعال شده باشد):";
+$lang["vector_cite_author"]           = "نام نویسنده در «یادکرد این مقاله«";
+$lang["vector_loaduserjs"]            = "آیا 'vector/user/user.js' را بارگذاری کند؟";
+$lang["vector_closedwiki"]            = " ویکی بسته‌شده (بیشتر پیوندها/برگه‌ها/جعبه‌ها تا زمانی که کاربر وارد حساب کاربری خود شود، به صورت مخفی است)";
