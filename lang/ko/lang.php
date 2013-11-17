070018a937c5003e434456c50a05cac5ab12cfca
Andreas Haerter <ah@foundata.net>
2013-11-17 13:50:46 +0100

    Add Korean language files

---
+++ lang/ko/lang.php
@@ -0,0 +1,114 @@
+<?php
+
+/**
+ * Korean language for the "vector" DokuWiki template
+ *
+ * If your language is not/only partially translated or you found an error/typo,
+ * have a look at the following files:
+ * - "/lib/tpl/vector/lang/<your lang>/lang.php"
+ * - "/lib/tpl/vector/lang/<your lang>/settings.php"
+ * If they are not existing, copy and translate the English ones (hint: looking
+ * at <http://[your lang].wikipedia.org> might be helpful).
+ *
+ * Don't forget to mail your translation to
+ *   Andreas Haerter <development@andreas-haerter.com>
+ * Thanks :-D.
+ *
+ *
+ * LICENSE: This file is open source software (OSS) and may be copied under
+ *          certain conditions. See COPYING file for details or try to contact
+ *          the author(s) of this file in doubt.
+ *
+ * @license GPLv2 (http://www.gnu.org/licenses/gpl2.html)
+ * @author 관인생략
+ * @author Myeongjin <aranet100@gmail.com>
+ * @link http://www.dokuwiki.org/template:vector
+ * @link http://www.dokuwiki.org/config:lang
+ * @link http://www.dokuwiki.org/devel:configuration
+ */
+
+
+//check if we are running within the DokuWiki environment
+if (!defined("DOKU_INC")){
+    die();
+}
+
+//tabs, personal tools and special links
+$lang["vector_article"] = "문서";
+$lang["vector_discussion"] = "토론";
+$lang["vector_read"] = "읽기";
+$lang["vector_edit"] = "편집";
+$lang["vector_create"] = "만들기";
+$lang["vector_userpage"] = "사용자 문서";
+$lang["vector_specialpage"] = "특수 문서";
+$lang["vector_mytalk"] = "사용자 토론";
+$lang["vector_exportodt"] = "내보내기: ODT";
+$lang["vector_exportpdf"] = "내보내기: PDF";
+$lang["vector_subscribens"] = "이름공간 바뀜 구독"; //original DW lang $lang["btn_subscribens"] is simply too long for common tab configs
+$lang["vector_unsubscribens"] = "이름공간 바뀜 구독 취소";  //original DW lang $lang["btn_unsubscribens"] is simply too long for common tab configs
+$lang["vector_translations"] = "언어";
+
+//headlines for the different bars and boxes
+$lang["vector_navigation"] = "둘러보기";
+$lang["vector_toolbox"] = "도구모음";
+$lang["vector_exportbox"] = "인쇄/내보내기";
+$lang["vector_qrcodebox"] = "QR 코드";
+$lang["vector_inotherlanguages"] = "언어";
+$lang["vector_printexport"] = "인쇄/내보내기";
+$lang["vector_personnaltools"] = "개인 도구";
+
+//buttons
+$lang["vector_btn_go"] = "보기";
+$lang["vector_btn_search"] = "찾기";
+$lang["vector_btn_search_title"] = "이 문자열이 포함된 문서 찾기";
+
+//exportbox ("print/export")
+$lang["vector_exportbxdef_print"] = "인쇄용 문서";
+$lang["vector_exportbxdef_downloadodt"] = "ODT로 다운로드";
+$lang["vector_exportbxdef_downloadpdf"] = "PDF로 다운로드";
+
+//default toolbox
+$lang["vector_toolbxdef_whatlinkshere"] = "여기를 가리키는 문서";
+$lang["vector_toolbxdef_upload"] = "파일 올리기";
+$lang["vector_toolbxdef_siteindex"] = "사이트맵";
+$lang["vector_toolboxdef_permanent"] = "고유 링크";
+$lang["vector_toolboxdef_cite"] = "이 문서 인용하기";
+
+//qr code box
+$lang["vector_qrcodebox_qrcode"] = "QR 코드";
+$lang["vector_qrcodebox_genforcurrentpage"] = "현재 문서의 생성된 QR 코드";
+$lang["vector_qrcodebox_urlofcurrentpage"] = "현재 문서의 QR 코드 (쉽게 모바일 접근을 하려면 스캔)";
+
+//cite this article
+$lang["vector_cite_bibdetailsfor"] = "다음 문서의 출처 정보:";
+$lang["vector_cite_pagename"] = "문서 이름";
+$lang["vector_cite_author"] = "저자";
+$lang["vector_cite_publisher"] = "발행처";
+$lang["vector_cite_dateofrev"] = "이 판의 날짜";
+$lang["vector_cite_dateretrieved"] = "확인한 날짜";
+$lang["vector_cite_permurl"] = "고유 URL";
+$lang["vector_cite_pageversionid"] = "문서 판 ID";
+$lang["vector_cite_citationstyles"] = "다음 문서의 인용 양식:";
+$lang["vector_cite_checkstandards"] = "필요에 따라 정확한 구문에 대한 양식 매뉴얼, 표준 가이드 또는 안내자의 지침을 확인하는 것을 기억하세요.";
+$lang["vector_cite_latexusepackagehint"] = "LaTeX 패키지 URL(프리앰블의 어딘가에 \usepackage{url})을 사용하면 더 정돈된 형식의 웹 주소를 얻을 수 있습니다. 다음과 같은 방법을 선호합니다";
+$lang["vector_cite_retrieved"] = "확인한 날짜:";
+$lang["vector_cite_from"] = "다음에서 찾아볼 수 있음:";
+$lang["vector_cite_in"] = "발행처:";
+$lang["vector_cite_accessed"] = "접근한 날짜:";
+$lang["vector_cite_cited"] = "인용한 날짜:";
+$lang["vector_cite_lastvisited"] = "마지막으로 방문한 날짜:";
+$lang["vector_cite_availableat"] = "다음에서 찾아볼 수 있음";
+$lang["vector_cite_discussionpages"] = "도쿠위키 토론 문서";
+$lang["vector_cite_markup"] = "문법";
+$lang["vector_cite_result"] = "결과";
+$lang["vector_cite_thisversion"] = "이 판";
+
+//other
+$lang["vector_search"] = "찾기";
+$lang["vector_accessdenied"] = "접근 거부됨";
+$lang["vector_fillplaceholder"] = "이 자리를 채우거나 비활성화하세요";
+$lang["vector_donate"] = "기부";
+$lang["vector_mdtemplatefordw"] = "도쿠위키를 위한 벡터 템플릿";
+$lang["vector_qrcodebtn"] = "QR 코드 생성기";
+$lang["vector_recentchanges"] = "최근 바뀜";
+
---
+++ lang/ko/settings.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * Korean language for the Config Manager
+ *
+ * If your language is not/only partially translated or you found an error/typo,
+ * have a look at the following files:
+ * - "/lib/tpl/vector/lang/<your lang>/lang.php"
+ * - "/lib/tpl/vector/lang/<your lang>/settings.php"
+ * If they are not existing, copy and translate the English ones (hint: looking
+ * at <http://[your lang].wikipedia.org> might be helpful).
+ *
+ * Don't forget to mail your translation to
+ *   Andreas Haerter <development@andreas-haerter.com>
+ * Thanks :-D.
+ *
+ *
+ * LICENSE: This file is open source software (OSS) and may be copied under
+ *          certain conditions. See COPYING file for details or try to contact
+ *          the author(s) of this file in doubt.
+ *
+ * @license GPLv2 (http://www.gnu.org/licenses/gpl2.html)
+ * @author Myeongjin <aranet100@gmail.com>
+ * @link http://www.dokuwiki.org/template:vector
+ * @link http://www.dokuwiki.org/config:lang
+ * @link http://www.dokuwiki.org/devel:configuration
+ */
+
+
+//check if we are running within the DokuWiki environment
+if (!defined("DOKU_INC")){
+    die();
+}
+
+//user pages
+$lang["vector_userpage"]    = "사용자 문서를 사용하겠습니까?";
+$lang["vector_userpage_ns"] = "만약 사용한다면 사용자 문서에 대한 루트로 다음 ':이름공간:' 사용:";
+
+//discussion pages
+$lang["vector_discuss"]    = "토론 탭/사이트를 사용하겠습니까?";
+$lang["vector_discuss_ns"] = "만약 사용한다면 토론에 대한 루트로 다음 ':이름공간:' 사용:";
+
+//site notice
+$lang["vector_sitenotice"]           = "사이트 넓은 알림을 보여줄까요?";
+$lang["vector_sitenotice_location"]  = "만약 보여준다면 사이트 넓은 알림에 대해 다음 위키 문서 사용:";
+$lang["vector_sitenotice_translate"] = "만약 보여주고 <a href=\"http://www.dokuwiki.org/plugin:translation\">Translation 플러그인</a>을 사용할 수 있다면: 언어별 사이트 넓은 알림을 불러올까요?<br />번역된 사이트 알림의 위키 문서는 ['vector_sitenotice_location'의 값]_[iso 언어 코드] (예를 들어 ':wiki:site_notice_de') 입니다.";
+
+//navigation
+$lang["vector_navigation"]           = "둘러보기를 보여줄까요?";
+$lang["vector_navigation_location"]  = "만약 보여준다면 둘러보기로 다음 위키 문서 사용:";
+$lang["vector_navigation_translate"] = "만약 보여주고 <a href=\"http://www.dokuwiki.org/plugin:translation\">Translation 플러그인</a>을 사용할 수 있다면: 언어별 둘러보기를 불러올까요?<br />번역된 둘러보기의 위키 문서는 ['vector_navigation_location'의 값]_[iso 언어 코드] (예를 들어 ':wiki:navigation_de') 입니다.";
+
+//exportbox ("print/export")
+$lang["vector_exportbox"]          = "'인쇄/내보내기' 모음를 보여줄까요?";
+$lang["vector_exportbox_default"]  = "만약 보여준다면 기본 '인쇄/내보내기' 모음을 사용하겠습니까?";
+$lang["vector_exportbox_location"] = "기본 모음을 사용하지 않는다면 '인쇄/내보내기' 모음 위치로 다음 위키 문서 사용:";
+
+//toolbox
+$lang["vector_toolbox"]          = "도구모음을 보여줄까요?";
+$lang["vector_toolbox_default"]  = "만약 보여준다면 기본 도구모음을 사용하겠습니까?";
+$lang["vector_toolbox_location"] = "기본 모음을 사용하지 않는다면 도구모음 위치로 다음 위키 문서 사용:";
+
+//qr code box
+$lang["vector_qrcodebox"] = "(모바일 브라우저에 쉽게 URL 전송을 위해) 현재 위키 문서 URL의 QR 코드로 된 상자를 보여줄까요?";
+
+//custom copyright notice
+$lang["vector_copyright"]           = "저작권 알림을 보여줄까요?";
+$lang["vector_copyright_default"]   = "만약 보여준다면 기본 저작권 알림을 사용하겠습니까?";
+$lang["vector_copyright_location"]  = "기본 알림을 사용하지 않는다면 저작권 알림으로 다음 위키 문서 사용:";
+$lang["vector_copyright_translate"] = "만약 보여주고 <a href=\"http://www.dokuwiki.org/plugin:translation\">Translation 플러그인</a>을 사용할 수 있다면: 언어별 저작권 알림을 불러올까요?<br />번역된 저작권 일림의 위키 문서는 ['vector_copyright_location'의 값]_[iso 언어 코드] (예를 들어 ':wiki:copyright_de') 입니다.";
+
+//donation link/button
+$lang["vector_donate"]          = "기부 링크/버튼을 보여줄까요?";
+$lang["vector_donate_url"]      = "기부에 대한 다음 URL 사용:";
+
+//TOC
+$lang["vector_toc_position"] = "목차 위치";
+
+//other stuff
+$lang["vector_breadcrumbs_position"]  = "(활성화되었다면) 위치 추적 둘러보기의 위치:";
+$lang["vector_youarehere_position"]   = "(활성화되었다면) '현재 위치' 둘러보기의 위치:";
+$lang["vector_cite_author"]           = "'이 문서 인용하기'의 저자 이름:";
+$lang["vector_loaduserjs"]            = "'vector/user/user.js'를 불러올까요?";
+$lang["vector_closedwiki"]            = "닫힌 위키입니까? (대부분의 링크/탭/도구를 사용자가 로그인하기 전에 숨깁니다)";
+
