825844a7629901c3eda3944a0fa7b404966a3434
nerun <danieldiasr@gmail.com>
2023-12-02 10:45:36 -0300

    Translation pt-br little fix.

--- lang/pt-br/settings.php
+++ lang/pt-br/settings.php
@@ -41,14 +41,14 @@ $lang["vector_discuss"]    = "Usar guias 'Discussão'?";
 $lang["vector_discuss_ns"] = "Se sim, usar o seguinte espaço nominal como raiz das páginas de discussão:";
 
 //site notice
-$lang["vector_sitenotice"]          = "Mostrar \"aviso do site\"?";
-$lang["vector_sitenotice_location"] = "Se sim, usar a seguinte página wiki:";
+$lang["vector_sitenotice"]           = "Mostrar \"aviso do site\"?";
+$lang["vector_sitenotice_location"]  = "Se sim, usar a seguinte página wiki:";
 $lang["vector_sitenotice_translate"] = "Se sim, e o <a href=\"https://www.dokuwiki.org/plugin:translation\">Translation plugin</a> estiver disponível: carregar aviso específico de idioma em todo o site?<br />A página wiki traduzida do \"aviso do site\" é [valor de 'vector_sitenotice_location']_[código de idioma ISO 639-1] (p.e. ':wiki:site_notice_pt').";
 
 //navigation
-$lang["vector_navigation"]          = "Mostrar 'Navegação' no menu?";
-$lang["vector_navigation_location"] = "Se sim, usar a seguinte página wiki para 'Navegação':";
-$lang["vector_navigation_translate"] = "Se sim, e o <a href=\"https://www.dokuwiki.org/plugin:translation\">Translation plugin</a> estiver disponível: carregar Navegação de idioma específico?<br />A página wiki traduzida da \"Navegação\" é [valor de 'vector_navigation_location']_[código de idioma ISO 639-1] (p.e. ':wiki:navigation_pt')."
+$lang["vector_navigation"]           = "Mostrar 'Navegação' no menu?";
+$lang["vector_navigation_location"]  = "Se sim, usar a seguinte página wiki para 'Navegação':";
+$lang["vector_navigation_translate"] = "Se sim, e o <a href=\"https://www.dokuwiki.org/plugin:translation\">Translation plugin</a> estiver disponível: carregar Navegação de idioma específico?<br />A página wiki traduzida da \"Navegação\" é [valor de 'vector_navigation_location']_[código de idioma ISO 639-1] (p.e. ':wiki:navigation_pt').";
 
 //exportbox ("print/export")
 $lang["vector_exportbox"]          = "Mostrar 'Imprimir/Exportar' no menu?";
@@ -64,9 +64,9 @@ $lang["vector_toolbox_location"] = "Se não usar o padrão, usar a seguinte pág
 $lang["vector_qrcodebox"] = "Mostrar caixa com o código QR do URL da página wiki atual (para facilitar a transferência do URL para o navegador do aparelho móvel)?";
 
 //custom copyright notice
-$lang["vector_copyright"]          = "Mostrar aviso de direitos autorais?";
-$lang["vector_copyright_default"]  = "Se sim, usar o padrão de direitos autorais?";
-$lang["vector_copyright_location"] = "Se não usar o padrão, usar a seguinte página wiki para direitos autorais:";
+$lang["vector_copyright"]           = "Mostrar aviso de direitos autorais?";
+$lang["vector_copyright_default"]   = "Se sim, usar o padrão de direitos autorais?";
+$lang["vector_copyright_location"]  = "Se não usar o padrão, usar a seguinte página wiki para direitos autorais:";
 $lang["vector_copyright_translate"] = "Se não usar o padrão e o <a href=\"https://www.dokuwiki.org/plugin:translation\">Translation plugin</a> estiver disponível: carregar aviso de direitos autorais específico para seu idioma?<br />A página wiki do aviso de direitos autorais traduzido é [valor de 'vector_copyright_location']_[código de idioma ISO 639-1] (e.g. ':wiki:copyright_pt').";
 
 //donation link/button
